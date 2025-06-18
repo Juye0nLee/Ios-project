f08218d19adf2f88bcec40c57ba56610a302a736
Juye0nLee <dlwndus708@naver.com>
2025-06-18 13:14:13 +0900

    Initial Commit

---
+++ TermProject-이주연-2191047/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TermProject-이주연-2191047
+//
+//  Created by 이주연 on 6/18/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
